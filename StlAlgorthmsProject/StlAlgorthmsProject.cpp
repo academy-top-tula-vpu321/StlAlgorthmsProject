b7999cbbc420f9eae0b284ad8c2f531a81127f0a
maxefimov <efimov.top.academy@gmail.com>
2023-12-24 13:11:13 +0300

    Добавьте файлы проекта.

---
+++ StlAlgorthmsProject/StlAlgorthmsProject.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+
+template <typename T>
+void ContainerInit(T& cont, int size)
+{
+    for (int i{}; i < size; i++)
+        cont.push_back(rand() % 100);
+}
+
+template <typename T>
+void ContainerPrint(T cont)
+{
+    std::for_each(cont.begin(), cont.end(), [](auto item)
+        {
+            std::cout << item << " ";
+        });
+    std::cout << "\n";
+}
+
+template <typename T>
+void ContainerPrintN(T cont, int count)
+{
+    std::for_each_n(cont.begin(), count, [](auto item)
+        {
+            std::cout << item << " ";
+        });
+    std::cout << "\n";
+}
+
+int main()
+{
+    srand(time(nullptr));
+
+    std::vector<int> v;
+    ContainerInit(v, 10);
+    ContainerPrint(v);
+
+    std::list<int> lst;
+    ContainerInit(lst, 10);
+    ContainerPrintN(lst, 5);
+}
